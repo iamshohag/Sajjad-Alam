879de36cc237b9a5f13847e585fa88cd7cfeb840
iamshohag <withoutyou.sk@gmail.com>
2022-03-05 20:02:26 +0600

    eighth commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+	// setTimeout(function(){
+    // 	$('#preloader').fadeToggle();
+    // }),1500;	
+	$(window).on("load",function(){
+		$('#preloader').fadeOut();
+	});
 $(document).ready(function(){
 	$(window).scroll(function(){
 		if(this.scrollY > 20){
@@ -50,10 +56,5 @@ $(document).ready(function(){
         }
     }
     });
-    // setTimeout(function(){
-    // 	$('#preloader').fadeToggle();
-    // }),1500;
-    $(window).on("load",function(){
-    	$('#preloader').fadeOut();
-    })
+    
 });
