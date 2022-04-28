4a09c86bb11ea1d95ebf78e018ba0bb2063f11c9
Sajjad Alam <89162164+iamshohag@users.noreply.github.com>
2022-04-28 20:19:41 +0600

    update index.js

--- index.js
+++ index.js
@@ -58,3 +58,20 @@ $(document).ready(function(){
     });
     
 });
+const counters = document.querySelectorAll('.counter');
+counters.forEach(counter =>{
+  counter.innerText = '0';
+  const updateCounter = () =>{
+    const target = counter.getAttribute('data-target');
+    const c = +counter.innerText;
+    const increment = target/1000;
+    if(c< target){
+      counter.innerText = `${Math.round(c+increment)}`;
+      setTimeout(updateCounter,1);
+    }
+    else{
+      counter.innerText = target;
+    }
+  }
+  updateCounter();
+})
